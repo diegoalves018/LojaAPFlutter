422cad71fcf2eb9ecd2a236e20a7d4a24cd7024d
Diego <diegoalvesupis@gmail.com>
2021-04-20 14:58:26 -0300

    Commit order_tab

--- lib/tabs/orders_tab.dart
+++ lib/tabs/orders_tab.dart
@@ -5,7 +5,6 @@ import 'package:loja_mobile/screens/login_screen.dart';
 import 'package:loja_mobile/tiles/order_tile.dart';
 
 class OrdersTab extends StatelessWidget {
-
   final ButtonStyle raisedButtonStyle = ElevatedButton.styleFrom(
     onPrimary: Colors.black87,
     primary: Color.fromARGB(255, 219, 64, 64),
@@ -18,60 +17,64 @@ class OrdersTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-   if(UserModel.of(context).isLoggedIn()){
+    if (UserModel.of(context).isLoggedIn()) {
+      String uid = UserModel.of(context).firebaseUser.uid;
 
-     String uid = UserModel.of(context).firebaseUser.uid;
-
-     return FutureBuilder<QuerySnapshot>(
-       future: Firestore.instance.collection("users").document(uid)
-       .collection("orders").getDocuments(),
-       builder: (context, snapshot) {
-         if (!snapshot.hasData)
-           return Center(
-             child: CircularProgressIndicator(),
-           );
-         else {
-           return ListView(
-             children: snapshot.data.documents.map((doc) =>
-                 OrderTile(doc.documentID)).toList(),
-           );
-         }
-       });
-
-   }else {
-     return Container(
-       padding: EdgeInsets.all(16.0),
-       child: Column(
-         crossAxisAlignment: CrossAxisAlignment.stretch,
-         mainAxisAlignment: MainAxisAlignment.center,
-         children: <Widget>[
-           Icon(
-             Icons.view_list_rounded,
-             size: 80.0,
-             color: Theme.of(context).primaryColor,
-           ),
-           SizedBox(
-             height: 16.0,
-           ),
-           Text(
-             "Faça o login para acompanhar seus pedidos!",
-             style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-             textAlign: TextAlign.center,
-           ),
-           SizedBox(
-             height: 16.0,
-           ),
-           ElevatedButton(
-             style: raisedButtonStyle,
-             child: Text('Entrar'),
-             onPressed: () {
-               Navigator.of(context).push(
-                   MaterialPageRoute(builder: (context) => LoginScreen()));
-             },
-           )
-         ],
-       ),
-     );
-   }
+      return FutureBuilder<QuerySnapshot>(
+          future: Firestore.instance
+              .collection("users")
+              .document(uid)
+              .collection("orders")
+              .getDocuments(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData)
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            else {
+              return ListView(
+                children: snapshot.data.documents
+                    .map((doc) => OrderTile(doc.documentID))
+                    .toList()
+                    .reversed
+                    .toList(),
+              );
+            }
+          });
+    } else {
+      return Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              Icons.view_list_rounded,
+              size: 80.0,
+              color: Theme.of(context).primaryColor,
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            Text(
+              "Faça o login para acompanhar seus pedidos!",
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            ElevatedButton(
+              style: raisedButtonStyle,
+              child: Text('Entrar'),
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => LoginScreen()));
+              },
+            )
+          ],
+        ),
+      );
+    }
   }
 }
